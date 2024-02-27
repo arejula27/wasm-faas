4d2b33f438b3194330f8306d2b605bc91d865a00
Íñigo Aréjula Aísa <arejula27@gmail.com>
2024-02-27 13:15:46 +0100

    init

--- internal/server/handler_test.go
+++ internal/server/handler_test.go
@@ -1,13 +1,14 @@
-package tests
+package server_test
 
 import (
 	"encoding/json"
 	"faas/internal/server"
-	"github.com/labstack/echo/v4"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
 	"testing"
+
+	"github.com/labstack/echo/v4"
 )
 
 func TestHandler(t *testing.T) {
